43e322a48a494581e2cd082cf402e9e558b0ac26
cstenkamp <529-cstenkamp@users.noreply.gitlab.informatik.uni-osnabrueck.de>
2020-01-17 14:42:36 +0100

    first commit

---
+++ botserver.py
@@ -0,0 +1,37 @@
+############################### externe imports ####################################
+from flask import Flask, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+####################################################################################
+
+app = Flask(__name__) #that's what's imported in the wsgi file
+app.config.from_object(__name__)
+app.config.update(
+    SESSION_COOKIE_NAME = 'session_medibot',
+    SESSION_COOKIE_PATH = '/medibot/'
+)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+settings.DBPATH+settings.DBNAME
+# db = SQLAlchemy(app)
+
+######################## interne imports, NACH creation der db #####################
+# from bothelper import handle_update, send_message
+
+####################################################################################
+
+@app.route("/", methods=["POST", "GET"])
+def update():
+    if request.method == 'POST':
+        update = request.data.decode("utf8")
+        update = json.loads(update)
+        # handle_update(update)
+        return "" #"" = 200 responsee
+    else:
+        return "This page is reserved for the Telegram-studIP-Bot (/)"
+
+
+####################################################################################
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
