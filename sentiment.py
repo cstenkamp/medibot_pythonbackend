944c970a67f70fb48904e1cdd9f919eca44db381
arontaupe <aron@petau.net>
2020-02-20 15:39:33 +0100

    plotting

--- sentiment.py
+++ sentiment.py
@@ -1,7 +1,7 @@
 import socket
 import tempfile
 from os.path import join, basename, abspath
-
+from math import pi
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -23,7 +23,16 @@ session = Session()
 
 
 def create_sentiment_graph(for_user, show_initial=True): #show_initial kann True, False, 2 sein. bei 2 zeigt's alle.
-    SENTIMENTS = ['happy', 'stressed', 'relaxed', 'lazy', 'bad'] #TODO die per API von den entities ziehen https://dialogflow.com/docs/reference/v2-auth-setup
+    SENTIMENTS = [ 'energized-neutral',
+                   'energized-pleasant',
+                   'neutral-pleasant',
+                   'calm-pleasant',
+                   'calm-neutral',
+                   'calm-unpleasant',
+                   'neutral-unpleasant',
+                   'energized-unpleasant'
+                   ]
+    #TODO die per API von den entities ziehen https://dialogflow.com/docs/reference/v2-auth-setup
     sentiment_dict = {key: [] for key in SENTIMENTS}
     qry = session.query(UserSentiment).filter(UserSentiment.user==for_user, UserSentiment.is_initial==int(show_initial)).all() if show_initial < 2 else \
             session.query(UserSentiment).filter(UserSentiment.user==for_user).all()
@@ -33,13 +42,24 @@ def create_sentiment_graph(for_user, show_initial=True): #show_initial kann True
             sentiment_dict[sent.sentiment].append(sent.strength)
         except KeyError:
             sentiment_dict[sent.sentiment] = [sent.strength]
-    averages = {key: sum(val)/len(val) if len(val) > 0 else 0 for key, val in sentiment_dict.items()}
+    averages = {key: sum(val)/len(val) if len(val) > 0 else 1  for key, val in sentiment_dict.items()}
+    attnr = len(SENTIMENTS)
+    angles = [n / float(attnr) * 2 * pi for n in range(attnr)]
+    angles += angles[:1]
+    angles = angles[:-1]
 
-    fig, ax = plt.subplots(1)
-    ax.bar(averages.keys(), averages.values(), 0.6, color='b')
+    val = list(averages.values())
+    fig, ax = plt.subplots(1, subplot_kw = dict(polar = True))
+    #ax = plt.subplot(111, polar=True)
+    plt.xticks(angles[:], SENTIMENTS)
+
+    ax.plot(angles, val)
+    ax.fill(angles, val, 'teal', alpha=0.2)
     ax.set(ylim=[0,7], title="Average "+("initial" if show_initial else "final")+" sentiment")
+
     filename = tempfile.mkstemp('.png')[1]
-    filename = join('/var/www/html/emotion_imgs', basename(filename)) #TODO den nicht als wert haben, und irgendwie für sicherheit hier sorgen, muss leider http sein
+    filename = join('/var/www/html/emotion_imgs', basename(filename))
+    #TODO den nicht als wert haben, und irgendwie für sicherheit hier sorgen, muss leider http sein
     if hostname in PRIVATE_HOSTNAMES:
         plt.show()
     else:
